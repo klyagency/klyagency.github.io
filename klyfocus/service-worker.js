1fb5932c7ef3bf7d8c3f8abd7eed8bd5f5b1d9f9
klyagency <contacto.klyagency@gmail.com>
2025-05-02 00:21:51 +0200

    Actualizar service-worker.js

--- klyfocus/service-worker.js
+++ klyfocus/service-worker.js
@@ -3,11 +3,10 @@ self.addEventListener('install', (event) => {
     caches.open('klyfocus-cache').then((cache) => {
       return cache.addAll([
         '/klyfocus/index.html',
-        '/logo.jpg',
-        '/manifest.json',
-        '/web-app-manifest-192×192.png',
-        '/web-app-manifest-512×512.png',
-        // Agrega más archivos que necesites cachear
+        '/klyfocus/logo.jpg',
+        '/klyfocus/manifest.json',
+        '/klyfocus/web-app-manifest-192x192.png',
+        '/klyfocus/web-app-manifest-512x512.png',
       ]);
     })
   );

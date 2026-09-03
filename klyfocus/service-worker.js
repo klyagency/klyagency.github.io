988f370bc12ae7a7e6ea84c2e478d23d26b56069
klyagency <contacto.klyagency@gmail.com>
2026-09-03 04:03:35 +0200

    Actualizar service-worker.js

--- klyfocus/service-worker.js
+++ klyfocus/service-worker.js
@@ -1,21 +1,27 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'gym-routine-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './web-app-manifest-192x192.png',
+  './web-app-manifest-512x512.png',
+  './logo.jpg'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('klyfocus-cache').then((cache) => {
-      return cache.addAll([
-        '/klyfocus/index.html',
-        '/klyfocus/logo.jpg',
-        '/klyfocus/manifest.json',
-        '/klyfocus/web-app-manifest-192x192.png',
-        '/klyfocus/web-app-manifest-512x512.png',
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
